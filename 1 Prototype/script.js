9430f772a2b96828b71c288c9b1b03608aaa4cac
randi089 <randifebriadi@gmail.com>
2024-06-21 09:16:22 +0700

    menggunakan class

--- 1 Prototype/script.js
+++ 1 Prototype/script.js
@@ -46,27 +46,53 @@
 // let randi = Mahasiswa('Randi', 10);
 // let tasya = Mahasiswa('Tasya', 20);
 
-function Mahasiswa(nama, energi) {
-    this.nama = nama;
-    this.energi = energi;
-}
+// function Mahasiswa(nama, energi) {
+//     this.nama = nama;
+//     this.energi = energi;
+// }
 
-Mahasiswa.prototype.makan = function (porsi) {
-    this.energi += porsi;
-    return `Halo ${this.nama}, selamat makan!`;
-}
+// Mahasiswa.prototype.makan = function (porsi) {
+//     this.energi += porsi;
+//     return `Halo ${this.nama}, selamat makan!`;
+// }
 
-Mahasiswa.prototype.main = function (jam) {
-    this.energi -= jam;
-    return `Halo ${this.nama}, selamat bermain!`;
-}
+// Mahasiswa.prototype.main = function (jam) {
+//     this.energi -= jam;
+//     return `Halo ${this.nama}, selamat bermain!`;
+// }
 
-Mahasiswa.prototype.tidur = function (jam) {
-    this.energi += jam*2;
-    return `Halo ${this.nama}, selamat tidur!`;
+// Mahasiswa.prototype.tidur = function (jam) {
+//     this.energi += jam*2;
+//     return `Halo ${this.nama}, selamat tidur!`;
+// }
+
+// let randi = new Mahasiswa('Randi', 10);
+
+// versi Class
+class Mahasiswa {
+    constructor(nama, energi) {
+        this.nama = nama;
+        this.energi = energi;
+    }
+
+    makan(porsi) {
+        this.energi += porsi;
+        return `Halo ${this.nama}, selamat makan!`;
+    }
+    
+    main(jam) {
+        this.energi -= jam;
+        return `Halo ${this.nama}, selamat bermain!`;
+    }
+    
+    tidur(jam) {
+        this.energi += jam*2;
+        return `Halo ${this.nama}, selamat tidur!`;
+    }    
 }
 
 let randi = new Mahasiswa('Randi', 10);
+let tasya = new Mahasiswa('Tasya', 20);
 
 // 3. Constructor Function
 // keyword new
