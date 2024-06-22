e94f0f329f96d6c32c1976a0e1ff52076ccb07de
randi089 <randifebriadi@gmail.com>
2024-06-22 09:31:25 +0700

    function factory menggunakan closure

--- 2 Closure/2script.js
+++ 2 Closure/2script.js
@@ -1,8 +1,23 @@
-function init() {
+// function init() {
+//     return function(nama) {
+//         console.log(nama);
+//     }
+// }
+// let panggilNama = init();
+// panggilNama('Randi');
+// panggilNama('Febriadi');
+
+function ucapkanSalam(waktu) {
     return function(nama) {
-        console.log(nama);
+        console.log(`Halo ${nama}, selamat ${waktu}, semoga harimu menyenangkan!`);
     }
 }
-let panggilNama = init();
-panggilNama('Randi');
-panggilNama('Febriadi');
+
+let selamatPagi = ucapkanSalam('Pagi');
+let selamatSiang = ucapkanSalam('Siang');
+let selamatMalam = ucapkanSalam('Malam');
+
+console.dir(selamatMalam('Randif'));
+
+selamatPagi('Randi');
+selamatMalam('Febriadi');
