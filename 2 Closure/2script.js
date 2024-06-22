b3e2d7607dfcccc6ac0914965f957c3cc5952b28
randi089 <randifebriadi@gmail.com>
2024-06-22 09:40:35 +0700

    counter  menggunakan closure

--- 2 Closure/2script.js
+++ 2 Closure/2script.js
@@ -7,17 +7,34 @@
 // panggilNama('Randi');
 // panggilNama('Febriadi');
 
-function ucapkanSalam(waktu) {
-    return function(nama) {
-        console.log(`Halo ${nama}, selamat ${waktu}, semoga harimu menyenangkan!`);
-    }
+// function ucapkanSalam(waktu) {
+//     return function(nama) {
+//         console.log(`Halo ${nama}, selamat ${waktu}, semoga harimu menyenangkan!`);
+//     }
+// }
+
+// let selamatPagi = ucapkanSalam('Pagi');
+// let selamatSiang = ucapkanSalam('Siang');
+// let selamatMalam = ucapkanSalam('Malam');
+
+// console.dir(selamatMalam('Randif'));
+
+// selamatPagi('Randi');
+// selamatMalam('Febriadi');
+
+let add = (function() {
+    let counter = 0;
+    return function() {
+    return ++counter;
 }
+})();
 
-let selamatPagi = ucapkanSalam('Pagi');
-let selamatSiang = ucapkanSalam('Siang');
-let selamatMalam = ucapkanSalam('Malam');
+counter = 10;
+counter = 100;
 
-console.dir(selamatMalam('Randif'));
-
-selamatPagi('Randi');
-selamatMalam('Febriadi');
+console.log(add());
+console.log(add());
+console.log(add());
+console.log(add());
+console.log(add());
+console.log(add());
