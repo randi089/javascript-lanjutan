cdb0e10ff52fb320a6da07129c9770633a8cc678
randi089 <randifebriadi@gmail.com>
2024-06-22 09:20:30 +0700

    closure1

---
+++ 2 Closure/2script.js
@@ -0,0 +1,10 @@
+function init() {
+    let nama = 'Randi';
+    let umur = 25;
+    function tampilNama() {
+        console.log(nama);
+        console.log(umur);
+    }
+    console.dir(tampilNama);
+}
+init();
